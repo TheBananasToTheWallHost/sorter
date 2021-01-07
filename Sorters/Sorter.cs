0ff5845bdd12c8f25d5b450a9aca463b1b5ffcaf
Ryan Mileris <ryanmileris@yahoo.com>
2021-01-06 20:11:18 -0700

    moved timsort related properties into Sorter.cs

--- Sorters/Sorter.cs
+++ Sorters/Sorter.cs
@@ -6,6 +6,14 @@ namespace Sorters
 {
     public partial class Sorter
     {
+        #region Fields/Properties
+        private static int timSortRunLength = 32;
+        public static int TimSortRunLength {
+            get => timSortRunLength;
+            set => timSortRunLength = value;
+        }
+        #endregion
+
         public static void HeapSort<T>(Span<T> span) where T : IComparable<T> {
 
         }
--- Sorters/TimSort.cs
+++ Sorters/TimSort.cs
@@ -8,20 +8,15 @@ namespace Sorters
 {
     public partial class Sorter
     {
-        private static int runLength = 32;
-        public static int TimSortRunLength {
-            get => runLength;
-            set => runLength = value;
-        }
         public static void TimSort<T>(Span<T> span) where T : IComparable<T> {
             
-            for(int i = 0; i < span.Length; i += runLength) {
-                int remaining = Math.Min(span.Length - i, runLength);
+            for(int i = 0; i < span.Length; i += timSortRunLength) {
+                int remaining = Math.Min(span.Length - i, timSortRunLength);
                 Span<T> spanSlice = span.Slice(i, remaining);
                 InsertionSort(spanSlice);
             }
 
-            for(int mergeSize = runLength; mergeSize < span.Length; mergeSize *= 2) {
+            for(int mergeSize = timSortRunLength; mergeSize < span.Length; mergeSize *= 2) {
                 for(int leftStart = 0; leftStart < span.Length; leftStart += 2 * mergeSize) {
 
                     int leftSize = Math.Min(mergeSize, span.Length - leftStart);
