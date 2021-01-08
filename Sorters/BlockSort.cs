3a0febe5cb70c03a9bb444d883b130a6a9a5aacd
Ryan Mileris <ryanmileris@yahoo.com>
2021-01-08 16:56:49 -0700

    added a cs file for block sort

---
+++ Sorters/BlockSort.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorters
+{
+    public partial class Sorter
+    {
+        public static void BlockSort<T>(Span<T> span) where T : IComparable<T> {
+            // TODO: will implement last
+        }
+
+        public static int FloorToPowerOfTwo(int x) {
+            for(int i = 1; i < 32; i*=2) {
+                x |= (x >> i);
+            }
+            return x - (x >> 1);
+        }
+    }
+}
