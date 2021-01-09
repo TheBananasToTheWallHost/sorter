357a5058b51e689bf87b758acc3be20c18e230b7
Ryan Mileris <ryanmileris@yahoo.com>
2021-01-08 19:53:42 -0700

    removed helper functions that are going to be placed in a separate project and made into a package

--- Sorters/BlockSort.cs
+++ Sorters/BlockSort.cs
@@ -11,12 +11,5 @@ namespace Sorters
         public static void BlockSort<T>(Span<T> span) where T : IComparable<T> {
             // TODO: will implement last
         }
-
-        public static int FloorToPowerOfTwo(int x) {
-            for(int i = 1; i < 32; i*=2) {
-                x |= (x >> i);
-            }
-            return x - (x >> 1);
-        }
     }
 }
